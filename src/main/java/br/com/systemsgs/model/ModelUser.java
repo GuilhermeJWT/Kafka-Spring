547a51602f7f9c6085a5006158dc9d83421b123a
GuilhermeJWT <guiromanno@gmail.com>
2021-09-18 11:39:09 -0300

    Iniciando o Model e as Configurações do Kafka

---
+++ src/main/java/br/com/systemsgs/model/ModelUser.java
@@ -0,0 +1,62 @@
+package br.com.systemsgs.model;
+
+import java.io.Serializable;
+
+public class ModelUser implements Serializable{
+
+	private static final long serialVersionUID = 1L;
+	
+	private Long id;
+	private String name;
+	private String[] address;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String[] getAddress() {
+		return address;
+	}
+
+	public void setAddress(String[] address) {
+		this.address = address;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		ModelUser other = (ModelUser) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+	
+}
